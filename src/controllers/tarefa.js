5a29d8d00e07a029932558b34cabec85728b943b
bruuno1994 <bruuno1015@gmail.com>
2023-04-20 19:29:05 -0300

    Try Catch implemented

--- src/controllers/tarefa.js
+++ src/controllers/tarefa.js
@@ -3,36 +3,56 @@ const service = require('../services/Tarefa')
 const controller = {}
 
 controller.create = async(request, response) => {
-    const tarefa = await service.create(request.body)
-    return response.json(tarefa)
+    try {
+        const tarefa = await service.create(request.body)
+        return response.json(tarefa)
+    } catch (error) {
+        return response.json(error)
+    }
 }
 
 controller.list = async(request, response) => {
-    const tarefa = await service.list()
-    return response.json(tarefa)
+    try {
+        const tarefa = await service.list()
+        return response.json(tarefa)
+    } catch (error) {
+        return response.json(error)
+    }
 }
 
 controller.getById = async(request, response) => {
-    const { id } = request.params 
-    const tarefa = await service.getById(Number(id))
-    
-        if(!tarefa) {
-            return response.json("Tarefa não encontrada")
-        } else {
-            return response.json(tarefa)
-        }
+    try {
+        const { id } = request.params 
+        const tarefa = await service.getById(Number(id))
+        
+            if(!tarefa) {
+                return response.json("Tarefa não encontrada")
+            } else {
+                return response.json(tarefa)
+            }
+    } catch (error) {
+        return response.json(error)
+    }
 }
 
 controller.delete = async(request, response) => {
-    const { id } = request.params
-    const tarefa = await service.delete(Number(id))
-    return response.json("Tarefa excluída com sucesso")
+    try {
+        const { id } = request.params
+        const tarefa = await service.delete(Number(id))
+        return response.json("Tarefa excluída com sucesso")
+    } catch (error) {
+        return response.json(error)
+    }
 }
 
 controller.update = async(request, response) => {
-    const { id } = request.params
-    const tarefa = await service.update(Number(id), request.body)
-    return response.json(tarefa)
+    try {
+        const { id } = request.params
+        const tarefa = await service.update(Number(id), request.body)
+        return response.json(tarefa)
+    } catch (error) {
+        return response.json(error)
+    }
 }
 
 module.exports = controller
